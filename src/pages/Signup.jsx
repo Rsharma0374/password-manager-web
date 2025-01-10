f24912ea8a5107d3dd6a634bab106104a4bc9252
rahulsharma <rsharma0374@gmail.com>
2025-01-11 01:29:24 +0530

    updated signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signup, sendEmailVerificationOTP, validateOTP } from '../services/authService'
-
+import CryptoJS from 'crypto-js';
 import { Eye, EyeOff, Mail, Lock, User, Calendar, UsersRound } from 'lucide-react';
 
 function Signup() {
@@ -10,7 +10,16 @@ function Signup() {
   const [otp, setOtp] = useState('');
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(''); // For showing error messages
+  const [successMessage, setSuccessMessage] = useState(''); // For showing success messages
   const [otpId, setOtpId] = useState('');
+  const [inputType, setInputType] = useState("text");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(true); // Tracks if passwords match
+  const [isPasswordValid, setIsPasswordValid] = useState(true); // Tracks if password meets criteria
+  const [isUsernameValid, setIsUsernameValid] = useState(true); // Tracks if username meets criteria
+
+
 
   const [formData, setFormData] = useState({
     name: '',
@@ -22,8 +31,15 @@ function Signup() {
     dateOfBirth: '',
   });
 
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  // Use useEffect to check if passwords match whenever the fields change
+  useEffect(() => {
+    if (formData.password === formData.confirmPassword || formData.confirmPassword === '') {
+      setPasswordsMatch(true);
+    } else {
+      setPasswordsMatch(false);
+    }
+  }, [formData.password, formData.confirmPassword]);
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -31,6 +47,25 @@ function Signup() {
       ...prev,
       [name]: value
     }));
+
+    if (name === 'password') {
+      setIsPasswordValid(value.length >= 8); // Check if password is at least 8 characters
+    }
+    if (name === 'username') {
+      setIsUsernameValid(value.length >= 8)
+    }
+  };
+
+  const handleOtpChange = (e) => {
+    setOtp(e.target.value);
+
+    // Set the input type to "text" when typing starts
+    setInputType("text");
+
+    // Convert to "password" after 2 seconds
+    setTimeout(() => {
+      setInputType("password");
+    }, 2000);
   };
 
   const handleSubmit = async (e) => {
@@ -65,22 +100,83 @@ function Signup() {
       setLoading(false);
     }
     // setStep(2); //for otp
-    console.log('Form submitted:', formData);
   };
 
   const handleOTPSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
+      const encryptedValue = CryptoJS.SHA1(otp + otpId).toString(CryptoJS.enc.Hex);
       // Add your signup logic here
       const res = await validateOTP(otp, otpId);
-      console.log(res);
+      if (res && res.oBody && res.oBody.payLoad && res.oBody.payLoad.sStatus === "SUCCESS" && res.oBody.payLoad.sEncryptedValue === encryptedValue) {
+        setSuccessMessage(res.oBody.payLoad.sResponse);
+        setTimeout(() => setSuccessMessage(''), 2000);
+        // Call the signup API
+        callSignupApi(formData);
+      } else if (res && res.aError && res.aError.length > 0) {
+        const error = res.aError[0];
+        if (error) {
+          setErrorMessage(error.sMessage);
+          setTimeout(() => setErrorMessage(''), 5000);
+          setLoading(false);
+        } else {
+          setErrorMessage("An unexpected error occurred. Please try again.");
+          setTimeout(() => setErrorMessage(''), 5000);
+          setLoading(false);
+        }
+      } else {
+        setErrorMessage("An unexpected error occurred. Please contact system administrator.");
+        setTimeout(() => setErrorMessage(''), 5000);
+        setLoading(false);
+      }
 
       // navigate('/'); // Navigate to login after successful verification
     } catch (error) {
       console.error('Error verifying OTP:', error);
     } finally {
+      // setLoading(false);
+    }
+  };
+
+  const callSignupApi = async (e) => {
+    setLoading(true);
+    setErrorMessage(''); // Clear any previous error messages
+    setSuccessMessage('')
+
+    try {
+      const res = await signup(formData);
+      if (res && res.oBody && res.oBody.payLoad && res.oBody.payLoad.sStatus === "SUCCESS") {
+
+        setSuccessMessage(res.oBody.payLoad.sResponseMessage);
+
+        // Keep loading true until navigation
+        setTimeout(() => {
+          setLoading(false);
+          navigate('/');
+        }, 5000);
+      } else if (res && res.aError && res.aError.length > 0) {
+        const error = res.aError[0];
+        if (error) {
+          setErrorMessage(error.sMessage);
+          setTimeout(() => setErrorMessage(''), 5000);
+        } else {
+          setErrorMessage("An unexpected error occurred. Please try again.");
+          setTimeout(() => setErrorMessage(''), 5000);
+        }
+        setLoading(false)
+      } else {
+        setErrorMessage("An unexpected error occurred. Please contact system administrator.");
+        setTimeout(() => setErrorMessage(''), 5000);
+        setLoading(false)
+      }
+
+    } catch (error) {
+      console.error('Error while signup api calling:', error);
+      setErrorMessage("An error occurred during signup. Please try again.");
+      setTimeout(() => setErrorMessage(''), 5000);
       setLoading(false);
+
     }
   };
 
@@ -101,6 +197,11 @@ function Signup() {
         {errorMessage && (
           <div className="mb-6 p-4 text-sm text-red-700 bg-red-100 rounded-lg">
             {errorMessage}
+          </div>
+        )}
+        {successMessage && (
+          <div className="mb-6 p-4 text-sm text-green-700 bg-green-100 rounder-lg">
+            {successMessage}
           </div>
         )}
 
@@ -133,6 +234,11 @@ function Signup() {
                 required
               />
             </div>
+            {!isUsernameValid && (
+              <div className="text-sm text-red-600 mt-2">
+                Username must be at least 8 characters long.
+              </div>
+            )}
 
             {/* Email Field */}
             <div className="relative">
@@ -168,6 +274,17 @@ function Signup() {
                 {showPassword ? <EyeOff className="h-5 w-5" /> : <Eye className="h-5 w-5" />}
               </button>
             </div>
+            {/* Show error message if passwords don't match or password is not 8 character */}
+            {!isPasswordValid && (
+              <div className="text-sm text-red-600 mt-2">
+                Password must be at least 8 characters long.
+              </div>
+            )}
+            {!passwordsMatch && (
+              <div className="text-sm text-red-600 mt-2">
+                Password and Confirm Password do not match.
+              </div>
+            )}
 
             {/* Confirm Password Field */}
             <div className="relative">
@@ -214,6 +331,7 @@ function Signup() {
                 name="dateOfBirth"
                 value={formData.dateOfBirth}
                 onChange={handleChange}
+                max={new Date().toISOString().split("T")[0]} // Restrict to past dates
                 className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
                 required
               />
@@ -221,8 +339,10 @@ function Signup() {
 
             <button
               type="submit"
-              disabled={loading}
-              className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+              disabled={!passwordsMatch || loading} // Disable button if passwords don't match or loading is true
+              // className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+              className={`w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors ${(!passwordsMatch || !isUsernameValid || loading) && 'opacity-50 cursor-not-allowed'
+                }`}
             >
               {loading ? 'Loading...' : 'Create Account'}
             </button>
@@ -231,11 +351,12 @@ function Signup() {
           <form onSubmit={handleOTPSubmit} className="space-y-4">
             <div className="relative">
               <input
-                type="text"
+                type={inputType}
                 placeholder="Enter OTP"
                 value={otp}
-                onChange={(e) => setOtp(e.target.value)}
+                onChange={handleOtpChange}
                 maxLength={6}
+                handleOtpChange
                 className="w-full px-4 py-2 text-center text-lg tracking-widest border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
                 required
               />
